8a97d6769cd86f1209d68f31806029122738e0f8
João Hitallo <joaohitallo97@gmail.com>
2023-01-20 04:49:58 -0300

    Criando summaryTable, Header e HabitDay - Aula 02

---
+++ web/src/components/Header.tsx
@@ -0,0 +1,20 @@
+import logoImage from '../assets/logo.png'
+import { Plus } from 'phosphor-react'
+
+
+export function Header (){
+  return (
+    <div className="w-full masx-w-3xl mx-auto flex items-center justify-between">
+      <img src={logoImage} alt="Habits"/>
+          
+      <button 
+      type='button'
+      className="border border-violet-500 font-semibold rounded-lg px--6 py-4 flex items-center gap-3 hover:border-violet-300"
+      > 
+        <Plus size={20} className="text-violet-500"/>
+        Novo Hábito
+      </button>
+      
+    </div>
+  )
+}
