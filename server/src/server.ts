81bf5531ec51f07bdf3cbc09e9cd78bf2cf8d747
João Hitallo <joaohitallo97@gmail.com>
2023-01-20 03:59:10 -0300

    Criando bd e rotas do server - aula 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,16 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
+
 
 const app = Fastify()
-const prisma = new PrismaClient()
-
 
 app.register(cors)
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany({
-
-  })
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
 }).then(() => {
-  console.log("listening on port 3334")
+  console.log("listening on port 3333")
 })
